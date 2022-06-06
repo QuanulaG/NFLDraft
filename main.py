d961562b4978edc3188cf4533c506856618dc2d8
QuanulaG <38017247+QuanulaG@users.noreply.github.com>
2022-06-06 02:21:19 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from typing import List, Any
+
+title: str = "NFL DraftPicks 2022"
+round_one = ["Travon Walker", "Aidan Hutchinson", "Derek Stingley Jr", "Ahmad Gardner", "Kayvon Thibodeaux", "Ikem Ekwonu", "Evan Neal", "Drake London", "Charles Cross", "Garrett Wilson", "Chris Olave", "Jameson Williams", "Jordan Davis", "Kyle Hamilton", "Kenyon Green", "Jahan Dotson.Zion Johnson", "Treylon Burks", "Trevor Penning", "Kenny Pickett", "Trent McDuffie", "Quay Walker", "Kaiir Elam", "Tyler Smith", "Tyler Linderbaum", "Jermaine Johnson", "Devin Lloyd", "Devontae Wyatt", "Cole Strange", "George Karlaftis", "Daxton Hill", "Lewis Cine"]
+round_two = ["Logan Hall", " ‘Christian Watson", "Roger McCreary", "Breece Hall", "Jalen Pitre", "Arnold Ebiketie", "Kyler Gordon", "Boye Mafe", "Kenneth Walker", "Andrew BoothJr", "WanDale Robinson", "John Metchie", "David Ojabo", "Josh Paschal", "Phidarian Mathis", "Jaquan Brisker", "Alontae Taylor", "Tyquan Thornton", "Cam Jurgens", "George Pickens", "Alec Pierce", "Skyy Moore", "Trey McBride", "Sam Williams", "Luke Goedeke", "Troy Andersen", "Ed Ingram", "Cam TaylorBritt", "Drake Jackson", "Bryan Cook", "James Cook", "Nik Bonitto"]
+round_three = ["Luke Fortner", "Brian Asamoah", "Josh Ezeudu", "Martin Emerson", "Nicholas PetitFrere", "Chad Muma", "Velus Jones", "Abraham Lucas", "Jelani Woods", "Desmond Ridder", "Christian Harris", "Travis Jones", "Bernhard Raimann", "Alex Wright", "JT Woods", "Greg Dulcich", "Cordale Flott", "DeAngelo Malone", "Nakobe Dean", "DeMarvin Leal", "Marcus Jones", "Malik Willis", "Cameron Thomas", "Jalen Tolbert", "Terrel Bernard", "Dylan Parham", "Rachaad White", "Sean Rhyan", "Ty DavisPrice", "Matt Corral", "Zachary Carter", "Nick Cross", "Kerby Joseph", "Brian Robinson", "David Bell", "Myjai Sanders", "Jeremy Ruckert", "Channing Tindall", "Leo Chenal", "Logan Bruss", "Danny Gray"]
+round_four = ["Cade Otten", "Dameon Pierce", "Perrion Winfrey", "Coby Bryant", "Daniel Faalele", "Max Mitchell", "Dan Bellinger", "Percy Butler", "Dane Belton", "Damarri Mathis", "Eyioma Uwazurike", "Micheal Clemons", "Akayleb Evans", "Jalyn ArmourDavis", "Brandon Smith", "Jack Jones", "Zamir White", "Isaiah Spiller", "Cade York", "Erik Ezukanma", "Neil Farrell Jr", "Pierre Strong Jr", "Charlie Kolar", "Jake Ferguson", "Jordan Stout", "Hassan Haskins", "Romeo Doubs", "Jake Camarda", "Spencer Burford", "Joshua Williams", "Cordell Volson", "Bailey Zappe", "Calvin Austin", "Isaiah Likely", "Zach Tom", "Damarion Williams", "Decobie Durant", "Chigoziem Okonkwo"]
+round_five = ["Sam Howell","Darian Kinnard", "Micah McFadden", "DJ Davidson", "Khalil Shakir", "Cole Turner", "Thomas Booker", "Tyler Allgeier", "Delarrin TurnerYell", "Tariq Wooden", "Snoop Conner", "Matt Waletzko", "Jerome Ford", "Sam Houston", "Zyon McCollum", "Tyreke Smith", "Eric Johnson", "Otito Ogbonnia", "DMarco Jackson", "Montrell Washington", "Kyle Philips", "Kyren Williams", "Esezi Otomewo", "Tycen Anderson", "DaRon Bland", "Braxton Jones", "Ty Chandler", "Teagan Quitoriano", "Luke Wattenberg", "Samuel Womack", "Marcus McKethan", "Dominique Robinson", "Matthew Butler", "Damone Clark", "James Mitchell", "John Ridgeway", "Kingsley Enagbare"]
+round_six = ["Matt Araiza", "Kyron Johnson", "Darrian Beavers", "Kevin Harris", "Vederian Lowe", "Christian Benford", "Zachary Thomas", "Nick Zakelj", "Malcolm Rodriguez", "Amare Barno", "Justin Shaffer", "Jalen Nailor", "Andrew Ogletree", "Devin Harper", "Jordan Jackson", "Jamaree Salyer", "Tyler Badie", "Gregory Junior", "Grant Calcaterra", "Cade Mays", "Sam Roberts", "Keaontay Ingram", "Michael Woods", "Trestan Ebner", "Theo Jackson", "Austin Deculus", "Matt Henningsen", "Doug Kramer", "Connor Heyward", "Luke Tenuta", "Chasen Hines", "Quentin Lake", "Derion Kendrick", "John Fitzpatrick", "JaSir Taylor", "Lecitus Smith", "Curtis Brooks", "Jackson James IV", "Ko Kieft", "Chance Campbell", "Kalia Davis", "Tariq CastroFields"]
+round_seven = ["Montaric Brown", "Isaiah Thomas", "Cameron Goode", "Mark Robinson", "JAtyre Carter", "Nick Muse", "Tariq Carpenter", "Bo Melton", "Chris Paul", "Baylon Spector", "Falon Hicks", "Dareke Young", "Jonathan Ford", "Daniel Hardy", "Deane Leonard", "Chase Lucas", "Thayer Munford", "Rodney Thomas", "Christian Holmes", "Chris Oladokun", "Kalon Barnes", "Jaylen Watson", "Christian Matthew", "Andrew Stueber", "Dawson Deaton", "Skylar Thompson", "Andre Anthony", "Rasheed Walker", "Brittain Brown", "Isaih Pacheco", "Jeffrey Gunter", "Russ Yeast", "Elijah Hicks", "Trenton Gill", "Jesse Luketa", "Marquis Hayes", " Samori Toure", "Nazeeh Johnson", "Zander Horvath", "AJ Arcuri", "Brock Purdy"]
+
+print(title)
+print("Round 1")
+print(round_one)
+print("Round 2")
+print(round_two)
+print("Round 3")
+print(round_three)
+print("Round 4")
+print(round_four)
+print("Round 5")
+print(round_five)
+print("Round 6")
+print(round_six)
+print("Round 7")
+print(round_seven)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
